5b5148e092381b3eb93cbaef46e1caa5ce90a5af
LucaGalofaro <lucagalofaro@outlook.it>
2025-01-26 11:20:09 +0100

    main menu background

--- Assets/_Project/Scripts/BubbleMoves.cs
+++ Assets/_Project/Scripts/BubbleMoves.cs
@@ -6,15 +6,18 @@ public class BubbleMoves : MonoBehaviour
     [SerializeField] float intensity = 25;
     RectTransform rect;
     Vector2 startPos;
+    float offset;
 
     private void Awake()
     {
+        offset = Random.Range(0f, 1f);
+        intensity += Random.Range(-intensity/2, intensity/2);
         rect = GetComponent<RectTransform>();
         startPos = rect.anchoredPosition;
     }
 
     void Update()
     {
-        rect.anchoredPosition = new Vector2(startPos.x , startPos.y + Mathf.Sin(Time.time * speed) * intensity);
+        rect.anchoredPosition = new Vector2(startPos.x , startPos.y + Mathf.Sin(Time.time * speed + offset) * intensity);
     }
 }
